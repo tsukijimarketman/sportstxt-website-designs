123b35b9cffd8d170937480614d9edd5554de619
Gerick M. Velasquez <teenbritish11@gmail.com>
2025-05-26 03:40:35 +0800

    fixes

--- Modern_Design/vite.config.ts
+++ Modern_Design/vite.config.ts
@@ -4,14 +4,15 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/sportstxt-website-designs/Modern_Design/', // <-- add this line
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
+    // mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
   resolve: {
@@ -19,4 +20,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
